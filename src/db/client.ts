44460d57129dbe733e1704f40c0db55324d41077
gm <guerau.march84@gmail.com>
2026-08-14 17:45:18 +0200

    feat: initialize Turso database client with read-write and read-only instances

--- src/db/client.ts
+++ src/db/client.ts
@@ -1,13 +1,25 @@
 import { createClient } from '@libsql/client'
 
+const url = process.env.TURSO_DATABASE_URL
+const authToken = process.env.TURSO_AUTH_TOKEN
+const authTokenReadOnly = process.env.TURSO_AUTH_TOKEN_READ_ONLY
+
+if (!url) {
+    throw new Error('Missing TURSO_DATABASE_URL')
+}
+
+if (!authToken) {
+    throw new Error('Missing TURSO_AUTH_TOKEN')
+}
+
 const readAndWriteDb = createClient({
-    url: process.env.TURSO_DATABASE_URL!,
-    authToken: process.env.TURSO_AUTH_TOKEN!,
+    url: url!,
+    authToken: authToken!,
 })
 
 const readOnlyDb = createClient({
-    url: process.env.TURSO_DATABASE_URL!,
-    authToken: process.env.TURSO_AUTH_TOKEN_READ_ONLY!,
+    url: url!,
+    authToken: authTokenReadOnly!,
 })
 
 export { readAndWriteDb, readOnlyDb }
